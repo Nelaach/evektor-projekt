a76ccd654b1ed86e7e16f21dc1a0527ef5545bf3
Verner Jan <jverner@evektor.cz>
2022-02-11 16:18:49 +0100

    Changes for ar-nexus deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,44 @@
 import React, { useState, useEffect } from 'react'
 
 function App() {
-  const [data, setData] = useState([{}])
+
+  const [data, setData] = useState([{}])  // data - actual variable, setData - function for manipulation data variable
+                                          // initial state is [{}]
+
   useEffect(() => {
-    fetch('users/').then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  })
+      fetch('/users', {
+          headers : { 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+          }
+      }).then(
+          res => {
+              console.log({ res });
+              return res.json();
+          }
+      ).then(
+          data => {
+            console.log("Data?")
+            setData(data)  // with 'setData' function we will fill 'data' variable of const [data, ...]
+            console.log({ data })
+          }
+      )
+  }, [])  // [] for it only run once 
+  
   return (
-    <div>
-      {(typeof data.members === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-        ))
-      )}
+      <div>
+
+        {(typeof data.zamestnanci === 'undefined') ? (
+            <p>Loading...</p>
+        ) : (
+            data.zamestnanci.map((emp, i) => (
+                <p key={i}>{emp}</p>
+            ))
+        )}
+	
     </div>
   );
 }
 
 export default App;
+
--- flask-server/server.py
+++ flask-server/server.py
@@ -3,7 +3,6 @@ from ldap3 import Server, Connection, ALL, SUBTREE
 from ldap3.core.exceptions import LDAPException, LDAPBindError
 from flask import Flask, render_template
 from dotenv import load_dotenv
-load_dotenv()
 import os
 from getpass import getuser
 import requests
@@ -11,8 +10,10 @@ import json
 
 app = Flask(__name__)
 
+load_dotenv()
+
 # ldap
-ldsp_server = os.getenv("LDSP_SERVER")
+ldsp_server = os.getenv("LDAP_SERVER")
 root_dn = os.getenv("ROOT_DN")
 ldap_user_name = os.getenv("LDAP_USER_NAME")
 ldap_password = os.getenv("LDAP_PASSWORD")
@@ -31,12 +32,14 @@ headers = {
 def home():
     return render_template('home.html', navbar = [current_user, isInGuesWifiGroup()])
 
-@app.route("/seznam")
+@app.route("/users")
 def seznam():
     r=requests.get(f"{snipeit}users/", headers = headers, verify=False)
     data = r.json()
     zamestnanci = data["rows"]
-    return render_template('seznam.html', navbar = [current_user, isInGuesWifiGroup()], zamestnanci = zamestnanci)
+    is_in_guestWifi_group = isInGuesWifiGroup()
+    return json.dumps(zamestnanci)
+    return render_template('seznam.html', navbar = [current_user, is_in_guestWifi_group], zamestnanci = zamestnanci)
 
 @app.route('/seznam/<id>')
 def zamestnanec(id):
@@ -63,18 +66,18 @@ def isInGuesWifiGroup():
                             user=ldap_user_name,
                             password=ldap_password,
                             auto_bind=True)
-
     connection.search(root_dn, f'(&(objectclass=user)(sAMAccountName={current_user}))', attributes=['memberOf','mail','uid'])
     HasAdmin = False
     for user in sorted(connection.entries):
-        print('uživatel: ', user)
         for group in user.memberOf:
             #print(group)
             if group.upper().find("CN="+"Guest_wifi".upper()) >= 0:
                 HasAdmin = True
 
     print("Je GuestWifi:", HasAdmin)
-    return(f"{HasAdmin}")
+    return HasAdmin
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='localhost', port=5000, debug=True)
+
+
